48d4007e0c5335a6ec5800e083d6e9a67a92a84e
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 00:03:13 +0100

    #4 Call the navigation bar in the home page php file

---
+++ src/pages/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Africa Géo-Junior</title>
+    <link rel="stylesheet" href="../assets/css/input.css">
+    <link rel="stylesheet" href="../assets/css/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    
+    <div class="flex flex-col min-h-[100vh]">
+        <?php include"../assets/components/navigation.php"; ?>
+    </div>
+
+</body>
+</html>
