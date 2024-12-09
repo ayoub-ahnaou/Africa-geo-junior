59ad84699cc05d87707065498d63ffae79bbe298
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-09 23:57:50 +0100

    #5 Make the navigation bar responsive with a burger menu in the side (still not working)

--- src/assets/components/navigation.php
+++ src/assets/components/navigation.php
@@ -1,4 +1,4 @@
-<nav class="flex items-center justify-between px-10 shadow-md text-gray-700 bg-gray-50 z-10">
+<nav class="flex items-center justify-between px-10  shadow-md text-gray-700 bg-gray-50 z-10">
     <ul class="h-full flex items-center">
         <li class="mr-8" href="./index.html">
         <a class="mr-8 flex items-center gap-1 cursor-pointer" href="./index.php">
@@ -7,7 +7,7 @@
         </a>
         </li>
     </ul>
-    <div class="flex items-center flex-1 justify-between text-sm">
+    <div class="flex items-center flex-1 justify-between text-sm max-md:hidden">
         <ul class="flex items-center">
             <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./dashboard.php">Dashboard Admin</a></li>
             <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="#">Explore Africa</a></li>
@@ -20,4 +20,5 @@
             <img src="../assets/images/icons/moon.svg" class="h-4 cursor-pointer" alt="">
         </div>
     </div>
+    <img src="../assets/images/icons/menu.svg" class="size-4 hidden max-md:block" alt="">
 </nav>
