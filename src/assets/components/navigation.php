8feac3457d34c10517870fe0a496a31d95ea92f9
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-09 23:56:48 +0100

    #5 Create the navigation bar interface

---
+++ src/assets/components/navigation.php
@@ -0,0 +1,23 @@
+<nav class="flex items-center justify-between px-10 shadow-md text-gray-700 bg-gray-50 z-10">
+    <ul class="h-full flex items-center">
+        <li class="mr-8" href="./index.html">
+        <a class="mr-8 flex items-center gap-1 cursor-pointer" href="./index.php">
+            <img src="../assets/images/icons/elephant.svg" class="size-8" alt="">
+            <span class="font-bold">Africa Géo-Junior</span>
+        </a>
+        </li>
+    </ul>
+    <div class="flex items-center flex-1 justify-between text-sm">
+        <ul class="flex items-center">
+            <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./dashboard.php">Dashboard Admin</a></li>
+            <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="#">Explore Africa</a></li>
+        </ul>
+        <div class="flex items-center h-full gap-4">
+            <div class="flex items-end hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear">
+                <span class="">Langues</span>
+                <img src="../assets/images/icons/chevron-down.svg" class="size-4" alt="">
+            </div>
+            <img src="../assets/images/icons/moon.svg" class="h-4 cursor-pointer" alt="">
+        </div>
+    </div>
+</nav>
