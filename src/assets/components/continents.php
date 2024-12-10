f2fee30e6576626162d4aced9c75ac5eadec94b5
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 10:45:38 +0100

    #6 Create the table that will store data comming from the continent table

---
+++ src/assets/components/continents.php
@@ -0,0 +1,21 @@
+<section class="w-4/5">
+    <h1 class="p-6 text-2xl">Continents</h1>
+    <div class="relative overflow-auto h-[70vh]">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th class="px-6 py-3">
+                        Continent ID
+                    </th>
+                    <th class="px-6 py-3">
+                        Name
+                    </th>
+                    <th class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+</section>
