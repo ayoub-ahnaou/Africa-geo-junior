46d9b9a3251799887eb57e17d399b41c9101b16d
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 11:15:16 +0100

    #8 Create the table of cities to store the data will comming from the ville table

---
+++ src/assets/components/cities.php
@@ -0,0 +1,30 @@
+<section class="w-4/5">
+    <h1 class="p-6 text-2xl">Cites</h1>
+    <div class="relative overflow-auto h-[70vh]">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-red-200">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th class="px-6 py-3">
+                        ville ID
+                    </th>
+                    <th class="px-6 py-3">
+                        Name
+                    </th>
+                    <th class="px-6 py-3">
+                        type
+                    </th>
+                    <th class="px-6 py-3">
+                        country
+                    </th>
+                    <th class="px-6 py-3">
+                        continent
+                    </th>
+                    <th class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+        </table>
+    </div>
+</section>
