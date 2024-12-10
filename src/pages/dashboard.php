d694ff002fe2abd4be28161f65481136fff06efe
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 10:29:46 +0100

    #24 Create the main body of dashboard page with choices in the left side

--- src/pages/dashboard.php
+++ src/pages/dashboard.php
@@ -15,6 +15,30 @@
     
     <div class="flex flex-col min-h-[100vh]">
         <?php include"../assets/components/navigation.php"; ?>
+
+        <div class="flex-grow flex">
+            <section class="w-1/5 bg-gray-100 flex flex-col">
+                <div class="flex py-6 px-2 items-center gap-1">
+                    <img src="../assets/images/icons/dashboard.svg" class="size-6" alt="">
+                    <h1 class="text-xl">Dashboard</h1>
+                </div>
+                <ul class="flex flex-col">
+                    <li class="p-2 hover:bg-white bg-white transition-all delay-75 ease-linear cursor-pointer flex items-center gap-2">
+                        <img src="../assets/images/icons/continent.svg" class="size-5" alt="">
+                        <p>Continents</p>
+                    </li>
+                    <li class="p-2 hover:bg-white transition-all delay-75 ease-linear cursor-pointer flex items-center gap-2">
+                        <img src="../assets/images/icons/countries.svg" class="size-5" alt="">
+                        <p>Country</p>
+                    </li>
+                    <li class="p-2 hover:bg-white transition-all delay-75 ease-linear cursor-pointer flex items-center gap-2">
+                        <img src="../assets/images/icons/cities.svg" class="size-5" alt="">
+                        <p>Cities</p>
+                    </li>
+                </ul>
+            </section>
+            <section class="w-4/5"></section>
+        </div>
         
         <?php include"../assets/components/footer.php"; ?> 
     </div>
