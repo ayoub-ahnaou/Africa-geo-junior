6d7f702da7f32cd8c8055aac117f2e15d4acbf99
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 11:04:20 +0100

    #9 Add the data of continent commnig from the db in the table dynamiclly

--- src/assets/components/continents.php
+++ src/assets/components/continents.php
@@ -3,7 +3,7 @@
         $sql = "SELECT * FROM continent";
         $result = mysqli_query($conn, $sql);
     ?>
-    
+
     <h1 class="p-6 text-2xl">Continents</h1>
     <div class="relative overflow-auto h-[70vh]">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -20,7 +20,30 @@
                     </th>
                 </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+                <?php
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>";
+                            echo "<th class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>";
+                            echo $row["id_continent"];
+                            echo "</th>";
+                            echo "<td class='px-6 py-2'>";
+                            echo $row["nom"];
+                            echo "</td>";
+                            echo "<td class='px-6 py-2 flex gap-2'>
+                                    <a href='#' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>
+                                    <a href='#' class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete</a>
+                                </td>";
+                            echo "</tr>";
+
+                            echo "";
+                        }
+                    } else {
+                        echo "<p class='text-red-600 bg-red-50 px-2'>No Continent is Found</p>";
+                    }
+                ?>
+            </tbody>
         </table>
     </div>
 </section>
