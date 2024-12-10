51c99123caa56be8e8f688fcdb569a0b722aed46
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 22:08:12 +0100

    #28 handle showing and hiding the form of add city with js

--- src/assets/js/form-handling.js
+++ src/assets/js/form-handling.js
@@ -11,6 +11,11 @@ const country_form = document.getElementById("country-form");
 const add_country_button = document.getElementById("add-country-button");
 const cancel_add_country = document.getElementById("cancel_add_country");
 
+// country form elements 
+const city_form = document.getElementById("city-form");
+const add_city_button = document.getElementById("add-city-button");
+const cancel_add_city = document.getElementById("cancel_add_city");
+
 function close_all_popup(){
     body.classList.remove("my-body-noscroll-class");
     background.classList.remove("blur");
@@ -18,8 +23,8 @@ function close_all_popup(){
     continent_form.classList.add("hidden");
     country_form.classList.remove("flex");
     country_form.classList.add("hidden");
-    // city_form.classList.remove("flex");
-    // city_form.classList.add("hidden");
+    city_form.classList.remove("flex");
+    city_form.classList.add("hidden");
 }
 
 // show the pop-up form of the continents
@@ -48,4 +53,18 @@ cancel_add_country.onclick = () => {
 }
 document.addEventListener("click", (e) => {
     if(e.target.id == "country-form") close_all_popup();
+})
+
+// show the pop-up form of the cities
+add_city_button.onclick = () => {
+    body.classList.add("my-body-noscroll-class");
+    background.classList.add("blur");
+    city_form.classList.add("flex");
+    city_form.classList.remove("hidden");
+};
+cancel_add_city.onclick = () => {
+    close_all_popup();
+}
+document.addEventListener("click", (e) => {
+    if(e.target.id == "city-form") close_all_popup();
 })
