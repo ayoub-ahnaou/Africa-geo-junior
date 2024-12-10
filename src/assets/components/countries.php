c12d2441c69a748065d2917bb4944267b317812d
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 11:10:38 +0100

    #9 Create the sql query to get data combined between two tables (pays, continent)

--- src/assets/components/countries.php
+++ src/assets/components/countries.php
@@ -1,4 +1,9 @@
 <section class="w-4/5">
+    <?php
+        $sql = "SELECT id_pays, pays.nom as nom, population, langues, continent.nom as continent FROM pays, continent where pays.id_continent = continent.id_continent ORDER by id_pays ASC";
+        $result = mysqli_query($conn, $sql);
+    ?>
+    
     <h1 class="p-6 text-2xl">Countries</h1>
     <div class="relative overflow-auto max-h-[70vh]">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
