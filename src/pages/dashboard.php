c75fcfc3634b75b40409fe0cd32fc8fa21161cd2
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 10:22:34 +0100

    #24 Create the dashboard page including the navigation and footer

---
+++ src/pages/dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin Page</title>
+    <link rel="stylesheet" href="../assets/css/input.css">
+    <link rel="stylesheet" href="../assets/css/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+</head>
+<body class="font-[Oswald]">
+    
+    <div class="flex flex-col min-h-[100vh]">
+        <?php include"../assets/components/navigation.php"; ?>
+        
+        <?php include"../assets/components/footer.php"; ?> 
+    </div>
+
+</body>
+</html>
