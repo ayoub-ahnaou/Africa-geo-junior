4001f96b6c49c2bc6bb763926a9f2bb913066d93
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 11:12:18 +0100

    #9 Fill the data of countries commnig from database in the table dynamicly with php

--- src/assets/components/countries.php
+++ src/assets/components/countries.php
@@ -29,7 +29,29 @@
                     </th>
                 </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+                
+                <?php
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>";
+                            echo "<th class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>".$row["id_pays"]."</th>";
+                            echo "<td class='px-6 py-2'>".$row["nom"]."</td>";
+                            echo "<td class='px-6 py-2'>".$row["population"]."</td>";
+                            echo "<td class='px-6 py-2'>".$row["langues"]."</td>";
+                            echo "<td class='px-6 py-2'>".$row["continent"]."</td>";
+                            echo "<td class='px-6 py-2 flex gap-2'>
+                                    <a href='#' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>
+                                    <a href='#' class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete</a>
+                                </td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<p class='text-red-600 bg-red-50 px-2'>No Continent is find</p>";
+                    }
+                ?>
+    
+            </tbody>
         </table>
     </div>
 </section>
