33b1e7f3425c02740c6ad3fa998538af0d058f69
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 21:32:44 +0100

    #25 Update and fix issue in the connection with database function

--- src/config/connection.php
+++ src/config/connection.php
@@ -1,11 +1,11 @@
 <?php 
-    define("HOST", "localhost");
-    define("USERNAME", "root");
-    define("PASSWORD", "");
-    define("DATABASE", "africa_geo_junior");
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "africa_geo_junior";
 
     try{
-        $conn = new mysqli(HOST, USERNAME, PASSWORD, DATABASE);
+        $conn = new mysqli($host, $username, $password, $database);
         echo "<p class='bg-green-50 text-green-600 px-2'>Connected to Database...</p>";
     }
     catch(Exception $err){
