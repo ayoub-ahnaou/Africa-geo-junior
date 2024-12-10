0c8675cf8d7dc7c913f228c48a9ddf4268738e9f
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-10 11:18:02 +0100

    #9 Get the cities data and store it in the table using php

--- src/assets/components/cities.php
+++ src/assets/components/cities.php
@@ -4,7 +4,7 @@
     $result = mysqli_query($conn, $sql);
 
     ?>
-    
+
     <h1 class="p-6 text-2xl">Cites</h1>
     <div class="relative overflow-auto h-[70vh]">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-red-200">
@@ -31,6 +31,28 @@
                 </tr>
             </thead>
             <tbody>
+                
+                <?php
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>";
+                            echo "<th class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>".$row["id_ville"]."</th>";
+                            echo "<td class='px-6 py-2'>".$row["nom"]."</td>";
+                            echo "<td class='px-6 py-2'>".$row["type"]."</td>";
+                            echo "<td class='px-6 py-2'>".$row["country"]."</td>";
+                            echo "<td class='px-6 py-2'>".$row["continent"]."</td>";
+                            echo "<td class='px-6 py-2 flex gap-2'>
+                                    <a href='#' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>
+                                    <a href='#' class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete</a>
+                                </td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<p class='text-red-600 bg-red-50 px-2'>No Continent is find</p>";
+                    }
+                ?>
+    
+            </tbody>
         </table>
     </div>
 </section>
