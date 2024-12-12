71fe3ba2592937fd1dd0100655b280c8554f64ef
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-12 21:48:36 +0100

    #10 Update button of add new city with the link of its page

--- src/assets/components/cities.php
+++ src/assets/components/cities.php
@@ -7,10 +7,12 @@
 
     <div class="p-4 flex justify-between items-center">
         <h1 class="text-2xl">Cities</h1>
-        <div id="add-city-button" class="flex items-center gap-1 cursor-pointer hover:bg-gray-100 p-2 px-4 rounded-md">
+        <!-- <div id="add-city-button" class="flex items-center gap-1 cursor-pointer hover:bg-gray-100 p-2 px-4 rounded-md"> -->
+        <a href="../../src/cruds/dashboard/create-cities.php" class="flex items-center gap-1 cursor-pointer hover:bg-gray-100 p-2 px-4 rounded-md">
             <span>Add New City</span>
             <img src="../assets/images/icons/add-record.svg" class="size-4" alt="">
-        </div>
+        </a>
+        <!-- </div> -->
     </div>
     <div class="relative overflow-auto h-[70vh]">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-red-200">
