e219dbb0728f0751d3c8daec63a7f8bd57ba64fa
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-13 20:46:53 +0100

    #4 Remove the message of succes in the connection with db

--- src/config/connection.php
+++ src/config/connection.php
@@ -6,7 +6,6 @@
 
     try{
         $conn = new mysqli($host, $username, $password, $database);
-        echo "<p class='bg-green-50 text-green-600 px-2'>Connected to Database...</p>";
     }
     catch(Exception $err){
         echo "<p class='bg-red-50 text-red-600 px-2'>Failed to Cennect to the Database... <br>". $err ."</p>";
